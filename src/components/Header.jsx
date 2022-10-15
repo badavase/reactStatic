e9032767ad51932572bd7cb0ac683ebd0ee893cb
Vasjan Çupri <102856709+v4sj4n@users.noreply.github.com>
2022-10-16 00:45:43 +0200

    Added Website

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import reactLogo from "./assets/react.svg";
+
+function Header() {
+  return (
+    <nav>
+      <img src={reactLogo} className="nav--icon" />
+      <h3 className="nav--logo_text">ReactFacts</h3>
+      <h4 className="nav--title">React Course - Project 1</h4>
+    </nav>
+  );
+}
+
+export default Header;
